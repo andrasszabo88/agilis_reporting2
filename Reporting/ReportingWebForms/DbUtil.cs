3dac19af7d0beb77db9c98cf857d6cb5d7ce46f7
andrasszabo88 <andrasszabo@outlook.com>
2013-11-27 21:46:38 +0100

    Extracted Open email count to DbUtil class

---
+++ Reporting/ReportingWebForms/DbUtil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReportingWebForms
+{
+    public class DbUtil
+    {
+        public int GetOpenedEmailCount(int campaignId)
+        {
+            using (var context = new Agilis_ReportingEntities())
+            {
+                int openedEmailCount = context.Response_Report.Count(p => p.open_date != null && p.campaign_id == campaignId);
+
+                return openedEmailCount;
+            }
+        }
+    }
+}
