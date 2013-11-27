e477866e84d83b1084864a51f56ada91550d31a9
andrasszabo88 <andrasszabo@outlook.com>
2013-11-27 21:05:44 +0100

    Initial commit

---
+++ Reporting/ReportingWebForms/Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ReportingWebForms.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <p>
+            <asp:Label Text="Customer name: " runat="server" />
+            <asp:TextBox ID="txtCustomerName" runat="server" />
+            <asp:Button ID="btnCustomerInput" runat="server" Text="Show my campaigns!" OnClick="btnCustomerInput_Click" />
+        </p>
+        <p runat="server" visible="false" id="campaignParagraph">
+            <asp:DropDownList ID="ddlCampaigns" runat="server" />
+            <asp:Button ID="btnQueryResponse" runat="server" Text="Show statistics!" OnClick="btnQueryResponse_Click" />
+            <asp:Button ID="btnQueryAdvanced" runat="server" Text="Show advanced statistics!" OnClick="btnQueryAdvanced_Click" />
+        </p>
+    </div>
+    <div id="results" runat="server">
+        <asp:Label ID="Label1" runat="server" Text="Opened/all email clicks ratio: " visible="false" />
+        <asp:Label ID="lblRatio" runat="server" Visible="false" />
+        <asp:Table ID="Table1" runat="server" Visible="false" />
+    </div>
+        
+    </form>
+</body>
+</html>
---
+++ Reporting/ReportingWebForms/Default.aspx.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ReportingWebForms
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCustomerInput_Click(object sender, EventArgs e)
+        {
+            campaignParagraph.Visible = false;
+            results.Visible = false;
+            ddlCampaigns.Items.Clear();
+
+            using (var ctx = new Agilis_ReportingEntities())
+            {
+                var query = from c in ctx.Campaigns
+                            where c.Customer.name == txtCustomerName.Text
+                            select c;
+
+                if (query.Count() == 0) return;
+
+                foreach (var item in query)
+                {
+                    ddlCampaigns.Items.Add(new ListItem(item.name, item.id.ToString()));
+                }
+
+                campaignParagraph.Visible = true;
+            }
+        }
+
+        protected void btnQueryResponse_Click(object sender, EventArgs e)
+        {
+            results.Visible = true;
+            Label1.Visible = true;
+            lblRatio.Visible = true;
+            int campaignId;
+            int.TryParse(ddlCampaigns.SelectedValue, out campaignId);
+
+            using(var ctx = new Agilis_ReportingEntities())
+            {
+                int opened = (from r in ctx.Response_Report
+                             where r.open_date != null && r.campaign_id == campaignId
+                             select r).Count();
+
+                int all = (from r in ctx.Response_Report
+                           where r.campaign_id == campaignId
+                           select r).Count();
+
+                if (all == 0) lblRatio.Text = "No emails have been sent in this campaign. Sorry :(";
+                else
+                {
+                    double openingStats = (double)opened / all * 100;
+                    lblRatio.Text = openingStats.ToString() + "%";
+                }
+            }
+        }
+
+        protected void btnQueryAdvanced_Click(object sender, EventArgs e)
+        {
+            Table1.Rows.Clear();
+            results.Visible = true;
+            int campaignId;
+            int.TryParse(ddlCampaigns.SelectedValue, out campaignId);
+
+            using (var ctx = new Agilis_ReportingEntities())
+            {
+                int opened = (from r in ctx.Response_Report
+                              where r.open_date != null && r.campaign_id == campaignId
+                              select r).Count();
+
+                var q = from r in ctx.Response_Report
+                        where r.open_date != null && r.campaign_id == campaignId
+                        group r by r.Device.name into g
+                        select new
+                        {
+                            name = g.Key,
+                            count = g.Count()
+                        };
+
+                if (q.Count() == 0) { Label1.Visible = true; lblRatio.Visible = true; lblRatio.Text = "No emails have been sent in this campaign. Sorry :("; }
+                else
+                {
+                    Label1.Visible = false;
+                    lblRatio.Visible = false;
+                    Table1.Visible = true;
+
+                    foreach (var device in q)
+                    {
+                        double openingStats = (double)device.count / opened * 100;
+                        TableRow r = new TableRow();
+                        r.Cells.Add(new TableCell() { Text = device.name+":" });
+                        r.Cells.Add(new TableCell() { Text = openingStats.ToString() + "%" });
+
+                        Table1.Rows.Add(r);
+                    }
+                }
+            }
+        }
+    }
+}
